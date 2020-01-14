00c3ec92e03d73cdd8225d7f55b5dcd629cc8c8d
keno <1211582428@qq.com>
2020-01-14 17:34:04 +0800

    webpack的loader - css-loader

---
+++ 02-webpack的loader/src/main.js
@@ -0,0 +1,12 @@
+import {add, mul} from './js/math'
+import * as perInfo from './js/info'  //将info的所有信息作为导入
+
+console.log(add(10, 20));
+console.log(mul(10, 20));
+
+console.log(perInfo.name);
+console.log(perInfo.age);
+console.log(perInfo.height);
+
+require('./css/normal.css')
+
