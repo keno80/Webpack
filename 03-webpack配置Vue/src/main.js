df8ea0c7ff193668c532f911af0a18c58c044b6b
keno <1211582428@qq.com>
2020-01-15 14:52:50 +0800

    webpack配置Vue

---
+++ 03-webpack配置Vue/src/main.js
@@ -0,0 +1,26 @@
+import {add, mul} from './js/math'
+import * as perInfo from './js/info'  //将info的所有信息作为导入
+
+console.log(add(10, 20));
+console.log(mul(10, 20));
+
+console.log(perInfo.name);
+console.log(perInfo.age);
+console.log(perInfo.height);
+
+//依赖CSS文件
+require('./css/normal.css')
+
+//依赖SCSS文件
+require('./css/spec.scss')
+
+//引入vue
+import Vue from 'vue'
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'hello word'
+  },
+
+})
+
---
+++ 03-webpack配置Vue/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+
+module.exports = {
+  //入口: 可以是字符串/数组/对象，因为这里只有一个，所以只需要一个字符串即可
+  entry: './src/main.js',
+  //出口: 通常是一个对象，里面至少包含两个重要的属性，path和filename
+  output: {
+    path: path.resolve(__dirname, 'dist'),  //path通常为一个绝对路径，可以使用node的path模块
+    filename: 'bundle.js',
+    publicPath: 'dist/'  //将打包后的文件都拼接一个dist/
+  },
+  module: {
+    rules: [
+      //当有多个的loader需要编译时，可以添加多个规则
+      {
+        test: /\.css$/,
+        //使用css-loader加载css，使用style-loader将样式使用到dom中
+        //使用多个loader时，从右向左
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            //limit：当文件大小小于limit规定的大小时，使用base64进行编译，当大于规定的大小时，使用file-loader进行加载
+            loader: 'url-loader',
+            options: {
+              limit: 140000,
+              //对命名做出限制，其中img/为文件夹，[name]为保存改文件或图片原有的名字，[hash:8]为保留hash值得八位，[ext]为保留原有的扩展名
+              name: 'img/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  // 将Vue的版本从runtime-only切换到runtime-compiler，以使vue可以正常编译
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
